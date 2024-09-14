a41f77b6c528813a21c4b156915e6ee2e0fc1f7f
sam <bafnasamidha@gmail.com>
2024-09-15 00:09:47 +0530

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react"; //import 'useState' hook from react
 
 const handleClick=(title_)=>{
-  document.title="daastaan-"+ title_;
+  document.title=title_+" - daastaan";
 }
 
 export default function Navbar() {
@@ -14,17 +14,17 @@ export default function Navbar() {
       {/* correct way to change the state */}
 
       <nav className="navigate">
-        <a className="navitem" href="#section1" onClick={handleClick("HOME")}>HOME</a>
-        <a className="navitem" href="#section2" onClick={handleClick("About")}>ABOUT US</a>
-        <a className="navitem" href="#section5" onClick={handleClick("Storyline")}>OUR STORY</a>
-        <a className="navitem" href="#section3" onClick={handleClick("Menu")}>MENU</a>
-        <a className="navitem" href="#section4" onClick={handleClick("Contact")}>CONTACT US</a>
+        <a className="navitem" href="#section1" onClick={()=>handleClick("Home")}>HOME</a>
+        <a className="navitem" href="#section2" onClick={()=>handleClick("About")}>ABOUT US</a>
+        <a className="navitem" href="#section5" onClick={()=>handleClick("Storyline")}>OUR STORY</a>
+        <a className="navitem" href="#section3" onClick={()=>handleClick("Menu")}>MENU</a>
+        <a className="navitem" href="#section4" onClick={()=>handleClick("Contact")}>CONTACT US</a>
 
        
 
         <div className="buttons">
-          <a className="button" href="/">Reservation</a>
-          <a className="button" href="/">Order Now</a>
+          <a className="button" href="/" onClick={()=>handleClick("Reservation")}>Reservation</a>
+          <a className="button" href="/" onClick={()=>handleClick("Order Online")}>Order Now</a>
         </div>
       </nav>
     </>
